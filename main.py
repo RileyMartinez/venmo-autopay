9be8a225d349510b396dfe8a236b2f34bd62b056
Riley Martinez <riley.m.martinez@gmail.com>
2023-07-24 20:47:13 -0500

    Add logging and git push actions

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
+import logging
 from venmo import Venmo
 from smtp_client import SmtpClient
 from datetime import datetime
 
 def main():
-    """Main function, all arguments are passed in via environment variables and secrets
-    """
     try:
+        logging.basicConfig(
+            filename='status.log', 
+            filemode='a', 
+            level=logging.DEBUG, 
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
         venmo = Venmo(os.getenv('VENMO_ACCESS_TOKEN'))
 
         smtp = SmtpClient(
@@ -20,17 +25,27 @@ def main():
             f'{os.getenv("VENMO_PAYMENT_NOTE")} {datetime.now().strftime("%m/%d/%y")}',
             os.getenv('VENMO_TARGET_USER_ID'),
             os.getenv('VENMO_FUNDING_SOURCE_ID'))
+        
+        logger.info(f'Venmo payment successful to Recipient ID: {mask_string(os.getenv("VENMO_TARGET_USER_ID"))}')
 
         if not success:
+            logger.error('Venmo payment failed')
+
             smtp.send_email(
                 os.getenv('SMTP_TO'),
                 os.getenv('SMTP_SUBJECT'),
                 os.getenv('SMTP_BODY'))
 
     except Exception as e:
+        logger.error(f'An exception was thrown')
+
         smtp.send_email(
             os.getenv('SMTP_TO'),
             os.getenv('SMTP_SUBJECT'),
             f'{os.getenv("SMTP_BODY")}\n\nError: {e}')
 
-main()
+def mask_string(s):
+    return 'X' * (len(s) - 4) + s[-4:]
+
+if __name__ == '__main__':
+    main()
